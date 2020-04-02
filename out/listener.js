63c47a39d11b2e49578cf9cc9d473a752adcec93
Bigciba <584665414@qq.com>
2020-04-02 19:10:10 +0800

    0.1.0

--- out/listener.js
+++ out/listener.js
@@ -9,7 +9,8 @@ const vscode = require("vscode");
 const init_1 = require("./init");
 class Listener {
     constructor() {
-        this.WatchAbilityExcel(); //监听excel
+        this.WatchAbilityExcel(); //监听技能excel
+        this.WatchUnitExcel(); //监听单位excel
         if (vscode.workspace.getConfiguration().get('dota2-tools.Listen Localization') === true) {
             this.WatchLocalization(); //监听文本合并
         }
@@ -61,6 +62,71 @@ class Listener {
                                         [col]: csv_arr[i + 1][j]
                                     };
                                     AbilitySpecial++;
+                                }
+                                else if (key === '') {
+                                    continue;
+                                }
+                                else {
+                                    values_obj[key] = col;
+                                }
+                            }
+                            i++;
+                            csv_data[row[0]] = values_obj;
+                        }
+                        fs.writeFileSync(kv_object[index], util.WriteKeyValue({ abilities: csv_data }));
+                    }
+                });
+            }
+        }
+    }
+    WatchUnitExcel() {
+        const excel_object = vscode.workspace.getConfiguration().get('dota2-tools.units_excel_path');
+        const kv_object = vscode.workspace.getConfiguration().get('dota2-tools.units_kv_path');
+        if (excel_object !== undefined && kv_object !== undefined) {
+            for (const index in excel_object) {
+                const listen_path = excel_object[index].replace(/\\\\/g, '/');
+                fs.watchFile(listen_path, (curr, prev) => {
+                    if (curr.nlink === 0) {
+                        console.log('removed');
+                    }
+                    else {
+                        console.log('changed');
+                        let sheet_list = node_xlsx_1.default.parse(listen_path);
+                        let csv = util.Array2CSV(sheet_list[0].data);
+                        let dir_name = path.dirname(listen_path);
+                        let file_name = listen_path.split(dir_name)[1].replace('/', '').split('\.')[0];
+                        util.DirExists(dir_name + '/csv');
+                        fs.writeFileSync(dir_name + '/csv/' + file_name + '.csv', '\uFEFF' + csv);
+                        // 生成kv
+                        let csv_data = {};
+                        let csv_arr = util.CSV2Array(csv);
+                        const csv_key = csv_arr[1];
+                        for (let i = 2; i < csv_arr.length; i++) {
+                            const row = csv_arr[i];
+                            if (row.length === 0) {
+                                continue;
+                            }
+                            let AttachWearables = 1;
+                            let values_obj = {
+                                Creature: {
+                                    AttachWearables: {}
+                                }
+                            };
+                            for (let j = 1; j < row.length; j++) {
+                                const col = row[j];
+                                // 跳过空值
+                                if (col === '') {
+                                    continue;
+                                }
+                                let key = csv_key[j];
+                                // special值特殊处理
+                                if (key === 'AttachWearables') {
+                                    key = AttachWearables.toString();
+                                    let value = col;
+                                    values_obj.Creature.AttachWearables[key] = {
+                                        ItemDef: value
+                                    };
+                                    AttachWearables++;
                                 }
                                 else if (key === '') {
                                     continue;
--- src/listener.ts
+++ src/listener.ts
@@ -8,7 +8,8 @@ import { Init,KV2LUA, VSND, GameDir } from './init';
 
 export class Listener {
 	constructor() {
-		this.WatchAbilityExcel();//监听excel
+		this.WatchAbilityExcel();	//监听技能excel
+		this.WatchUnitExcel();		//监听单位excel
 		if (vscode.workspace.getConfiguration().get('dota2-tools.Listen Localization') === true) {
 			this.WatchLocalization();//监听文本合并
 		}
@@ -60,6 +61,69 @@ export class Listener {
 										[col]: csv_arr[i+1][j]
 									};
 									AbilitySpecial++;
+								} else if (key === '') {
+									continue;
+								} else {
+									values_obj[key] = col;
+								}
+							}
+							i++;
+							csv_data[row[0]] = values_obj;
+						}
+						fs.writeFileSync(kv_object[index], util.WriteKeyValue({abilities:csv_data}));
+					}
+				});
+			}
+		}
+	}
+	WatchUnitExcel() {
+		const excel_object: util.Configuration|undefined = vscode.workspace.getConfiguration().get('dota2-tools.units_excel_path');
+		const kv_object: util.Configuration|undefined = vscode.workspace.getConfiguration().get('dota2-tools.units_kv_path');
+		if (excel_object !== undefined && kv_object !== undefined) {
+			for (const index in excel_object) {
+				const listen_path = excel_object[index].replace(/\\\\/g,'/');
+				fs.watchFile(listen_path, (curr, prev) => {
+					if (curr.nlink === 0) {
+						console.log('removed');
+					} else {
+						console.log('changed');
+						let sheet_list: any = xlsx.parse(listen_path);
+						let csv: string = util.Array2CSV(sheet_list[0].data);
+						let dir_name: string = path.dirname(listen_path);
+						let file_name: string = listen_path.split(dir_name)[1].replace('/','').split('\.')[0];
+						util.DirExists(dir_name + '/csv');
+						fs.writeFileSync(dir_name + '/csv/' + file_name + '.csv', '\uFEFF' + csv);
+						// 生成kv
+						let csv_data:any = {};
+						let csv_arr:any = util.CSV2Array(csv);
+						const csv_key:[] = csv_arr[1];
+						for (let i = 2; i < csv_arr.length; i++) {
+							const row:any = csv_arr[i];
+							if (row.length === 0) {
+								continue;
+							}
+	
+							let AttachWearables:number = 1;
+							let values_obj: any = {
+								Creature:{
+									AttachWearables:{}
+								}
+							};
+							for (let j = 1; j < row.length; j++) {
+								const col = row[j];
+								// 跳过空值
+								if (col === '') {
+									continue;
+								}
+								let key:string = csv_key[j];
+								// special值特殊处理
+								if (key === 'AttachWearables') {
+									key = AttachWearables.toString();
+									let value = col;
+									values_obj.Creature.AttachWearables[key] = {
+										ItemDef:value
+									};
+									AttachWearables++;
 								} else if (key === '') {
 									continue;
 								} else {
