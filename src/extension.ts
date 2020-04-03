9507ed8f1bccec4ff86283c3455d291dae3372d0
Bigciba <584665414@qq.com>
2020-04-03 19:33:05 +0800

    readme

--- src/extension.ts
+++ src/extension.ts
@@ -1458,15 +1458,6 @@ export async function activate(context: vscode.ExtensionContext) {
 			let texture: string = message.replace(/_png\.png/, '');
 			vscode.env.clipboard.writeText(texture);
 			util.ShowInfo('已将图标路径复制到剪切板');
-			// vscode.window.activeTextEditor?.edit(editBuilder =>{
-			// 	if (vscode.window.activeTextEditor?.selection.start !== undefined && texture !== undefined) {
-			// 		if (vscode.window.activeTextEditor.selection.start.character === vscode.window.activeTextEditor.selection.end.character) {
-			// 			editBuilder.insert(vscode.window.activeTextEditor?.selection.start, texture);
-			// 		} else {
-			// 			editBuilder.replace(new vscode.Range(vscode.window.activeTextEditor?.selection.start, vscode.window.activeTextEditor?.selection.end), texture);
-			// 		}
-			// 	}
-			// });
 			panel.dispose();
 		}, undefined, context.subscriptions);
 	});
