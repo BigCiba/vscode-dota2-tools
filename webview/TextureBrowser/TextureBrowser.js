d49a80c4c805df267da63fa4c8e342bfb984f5b5
Bigciba <584665414@qq.com>
2020-12-29 17:51:57 +0800

    物品图标加上item_前缀

--- webview/TextureBrowser/TextureBrowser.js
+++ webview/TextureBrowser/TextureBrowser.js
@@ -4,21 +4,21 @@ const textureContainer = (document.querySelector('.texture-content'));
 const itemTextureContainer = (document.querySelector('.item-texture-content'));
 const dropdownContainer = (document.querySelector('.dropdown-content'));
 const heroFilter = (document.querySelector('.hero-filter'));
-const filter = document.getElementById("filter")
+const filter = document.getElementById("filter");
 
 function OnInput() {
-	let container = (textureContainer.style.display == 'grid' || textureContainer.style.display == '') ? textureContainer:itemTextureContainer;
+	let container = (textureContainer.style.display == 'grid' || textureContainer.style.display == '') ? textureContainer : itemTextureContainer;
 	let children = container.children;
 	let filter_word = filter.value;
 	for (let index = 0; index < children.length; index++) {
 		const element = children[index];
 		if (filter_word != '' && element.id.search(filter_word) == -1) {
-			element.style.display = 'none'
+			element.style.display = 'none';
 		} else {
 			element.style.display = '';
 		}
 	}
-	
+
 	// vscode.postMessage(image.id);
 }
 function ToggleType(btn) {
@@ -37,7 +37,7 @@ function ToggleType(btn) {
 			textureContainer.style.display = 'none';
 			itemTextureContainer.style.display = 'grid';
 			if (itemTextureContainer.bInit == undefined) {
-				itemTextureContainer.bInit = true
+				itemTextureContainer.bInit = true;
 				InitItemContent(state.icons_data);
 			}
 		}
@@ -91,7 +91,7 @@ function InitAbilityContent(icons_data) {
 					id: id,
 				});
 			});
-	
+
 			textureContainer.appendChild(img);
 		}
 	}
@@ -141,16 +141,16 @@ function InitItemContent(icons_data) {
 				vscode.postMessage({
 					event: 'click',
 					type: 'items',
-					id: id,
+					id: "item_" + id,
 				});
 			});
-	
+
 			itemTextureContainer.appendChild(img);
 		}
 	}
 }
 function InitHeroFilter(icons_data) {
-	let heroes_data = icons_data.heroes
+	let heroes_data = icons_data.heroes;
 	let root_path = heroes_data.path;
 	let data = heroes_data.data;
 	console.log(data);
@@ -179,11 +179,11 @@ window.addEventListener('message', event => {
 			const icons_data = message.icons_data;
 			InitAbilityContent(icons_data);
 			InitHeroFilter(icons_data);
-			
+
 			// updateContent(texture_data, spellicons_uri, heroes_data, heroes_icon_uri,custom_spellicons_data,custom_spellicons_uri);
 			// Then persist state information.
 			// This state is returned in the call to `vscode.getState` below when a webview is reloaded.
-			vscode.setState({ 
+			vscode.setState({
 				icons_data,
 			});
 
