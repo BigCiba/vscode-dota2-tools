d9a6ccb4aa10bcbf0fd4f4fe96d086faacb09885
Ninja-EOM-PC <576385849@qq.com>
2022-07-04 19:51:35 +0800

    excel快速打开

--- src/extension.ts
+++ src/extension.ts
@@ -25,6 +25,7 @@ import { statusBarItemInit } from './module/statusBar';
 import { preProcessing } from './command/cmdPreProcessing';
 import { quickStart } from './command/cmdQuickStart';
 import { vmdlEditorProvider } from './CustomTextEditorProvider/vmdlEditorProvider';
+import { excelEditorProvider } from './CustomTextEditorProvider/ExcelEditorProvider';
 
 export async function activate(context: vscode.ExtensionContext) {
 	// 基础模块单独载入
@@ -67,6 +68,7 @@ export async function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.commands.registerCommand('dota2tools.preProcessing', () => preProcessing(context)));
 	context.subscriptions.push(vscode.commands.registerCommand('dota2tools.welcomePage', (tag) => quickStart(context, tag)));
 	context.subscriptions.push(vmdlEditorProvider.register(context));
+	context.subscriptions.push(vscode.window.registerCustomEditorProvider("dota2tools.excelLazyboy", new excelEditorProvider()));
 }
 
 export function deactivate() { }
