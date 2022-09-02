e10e89e8e334d96c104fe9570dd9c96e5e73486d
Bigciba <584665414@qq.com>
2022-09-02 14:06:44 +0800

    更新版本

--- out/command/cmdPreProcessing.js
+++ out/command/cmdPreProcessing.js
@@ -4,13 +4,21 @@ exports.preProcessing = void 0;
 const preProcessing_1 = require("../module/preProcessing");
 async function preProcessing(context) {
     (0, preProcessing_1.itemsGameParse)(context);
+    console.log("itemsGameParse");
     (0, preProcessing_1.parsePanoramaAPI)(context);
+    console.log("parsePanoramaAPI");
     (0, preProcessing_1.parseCssDocument)(context);
+    console.log("parseCssDocument");
     (0, preProcessing_1.parseEventDocument)(context);
+    console.log("parseEventDocument");
     (0, preProcessing_1.parsePanelList)(context);
+    console.log("parsePanelList");
     (0, preProcessing_1.vsndGenerator)(context);
+    console.log("vsndGenerator");
     (0, preProcessing_1.parseLuaAPI)(context);
+    console.log("parseLuaAPI");
     (0, preProcessing_1.parseLuaAPIChangelog)(context);
+    console.log("parseLuaAPIChangelog");
     // rogueItemsGameParse(context);
 }
 exports.preProcessing = preProcessing;
