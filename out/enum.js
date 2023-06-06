a7ed8dd77a7e6dd2f2e35ed1031ebe91e1e3a813
Bigciba <584665414@qq.com>
2023-06-06 11:44:19 +0800

    傻逼配置

--- out/enum.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ENUM_STATUS_BAR_MESSAGE_TIMEOUT = void 0;
-/** 消息栏信息过期时间 */
-exports.ENUM_STATUS_BAR_MESSAGE_TIMEOUT = 5;
-//# sourceMappingURL=enum.js.map
