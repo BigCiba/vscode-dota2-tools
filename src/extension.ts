588db9f76a1b54f0b3b9cc551a9f6f364bec390b
Ninja-EOM-PC <576385849@qq.com>
2023-06-25 16:44:26 +0800

    修正继承表功能，移除完全没用的功能

--- src/extension.ts
+++ src/extension.ts
@@ -20,7 +20,6 @@ import { GenerateDBCSV } from "./command/cmdGenerateDBCSV";
 import { generateLuaApiDocument } from './command/cmdGenerateLuaApiDocument';
 import { generateVPDI } from './command/cmdGenerateVPDI';
 import { getItemRemoveList } from './command/cmdGetItemRemoveList';
-import { inheritCSV } from './command/cmdInheritCSV';
 import { inheritTable } from './command/cmdInheritTable';
 import { abilityExport, unitExport } from './command/cmdKvExport';
 import { kvToJs } from './command/cmdKvToJs';
@@ -59,8 +58,6 @@ export async function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.commands.registerCommand('dota2tools.kv_to_js_config', () => kvToJs(context)));
 	context.subscriptions.push(vscode.commands.registerCommand('dota2tools.default_item_remove_list', () => getItemRemoveList(context)));
 	context.subscriptions.push(vscode.commands.registerCommand('dota2tools.inherit_table', () => inheritTable(context)));
-	context.subscriptions.push(vscode.commands.registerCommand('dota2tools.localization_csv', () => inheritTable(context)));
-	context.subscriptions.push(vscode.commands.registerCommand('dota2tools.localization_text', () => inheritTable(context)));
 	context.subscriptions.push(vscode.commands.registerCommand('dota2tools.samsara_hero_drop', () => dropVPCf(context)));
 	context.subscriptions.push(vscode.commands.registerCommand('dota2tools.generate_lua_api_document', () => generateLuaApiDocument(context)));
 	context.subscriptions.push(vscode.commands.registerCommand('dota2tools.create_wearable_items', () => createWearableItems(context)));
@@ -73,7 +70,6 @@ export async function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.commands.registerCommand('dota2tools.OpenKV', () => openKV(context)));
 	context.subscriptions.push(vscode.commands.registerCommand('dota2tools.Localization', () => combineLocalization()));
 	context.subscriptions.push(vscode.commands.registerCommand('dota2tools.excel2kv', () => cmdExcel2KV(context)));
-	context.subscriptions.push(vscode.commands.registerCommand('dota2tools.inheritCSV', () => inheritCSV(context)));
 	context.subscriptions.push(vscode.commands.registerCommand('dota2tools.ability_export', (uri) => abilityExport(context, uri)));
 	context.subscriptions.push(vscode.commands.registerCommand('dota2tools.unit_export', (uri) => unitExport(context, uri)));
 	context.subscriptions.push(vscode.commands.registerCommand('dota2tools.preProcessing', () => preProcessing(context)));
