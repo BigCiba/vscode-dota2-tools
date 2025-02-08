268094bfee8570056780adacc7f8ce0f77bc845e
Ninja-EOM-PC <576385849@qq.com>
2025-02-08 11:44:19 +0800

    增加一些错误提示

--- src/command/cmdDota2mklink.ts
+++ src/command/cmdDota2mklink.ts
@@ -4,7 +4,7 @@ import { moveSync, pathExistsSync } from "fs-extra";
 import { ExtensionContext, workspace } from "vscode";
 import { StopAllListener, TryStartWatch } from "../listener/common";
 import { getContentDir, getGameDir } from "../module/addonInfo";
-import { changeStatusBarState, StatusBarState } from "../module/statusBar";
+import { changeStatusBarState, showStatusBarMessage, StatusBarState } from "../module/statusBar";
 import { getRootPath } from "../utils/getRootPath";
 import path = require("path");
 
@@ -12,6 +12,11 @@ export async function mklinkForDota2Addon(context: ExtensionContext) {
 	// const sDotaDir = await GetSteamAPPIntallDirByID("570");
 	const sDotaDir = workspace.getConfiguration().get<string>("dota2-tools.dota2_install_path");
 	if (sDotaDir) {
+		if (!pathExistsSync(sDotaDir)) {
+			showStatusBarMessage("请先配置正确的Dota2安装目录");
+			return;
+		}
+
 		changeStatusBarState(StatusBarState.LOADING);
 		const contentDir = getContentDir();
 		const gameDir = getGameDir();
@@ -22,6 +27,18 @@ export async function mklinkForDota2Addon(context: ExtensionContext) {
 		const sDotaGameAddon = path.join(sDotaDir, "game", "dota_addons");
 		const sDotaContentDir = path.join(sDotaContentAddon, sGameName);
 		const sDotaGameDir = path.join(sDotaGameAddon, sGameName);
+
+		if (!pathExistsSync(sDotaContentAddon)) {
+			changeStatusBarState(StatusBarState.ALL_DONE);
+			showStatusBarMessage("Dota2 content 目录不存在");
+			return;
+		}
+
+		if (!pathExistsSync(sDotaGameAddon)) {
+			changeStatusBarState(StatusBarState.ALL_DONE);
+			showStatusBarMessage("Dota2 game 目录不存在");
+			return;
+		}
 
 		let bContentNotExist = !pathExistsSync(sDotaContentDir);
 		let bGameNotExist = !pathExistsSync(sDotaGameDir);
