61f0eb61acc30b3f152371ec86192b2beb0cb6e2
bigciba <584665414@qq.com>
2025-02-25 14:44:05 +0800

    tree view

--- src/TreeDataProvider/kvTree.ts
+++ src/TreeDataProvider/kvTree.ts
@@ -1,28 +1,40 @@
-import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
-
-
+import * as vscode from 'vscode';
+import { getGameDir } from '../module/addonInfo';
+import { readKeyValue2 } from '../utils/kvUtils';
 
 export class KvEditorTreeProvider implements vscode.TreeDataProvider<NodeItem> {
 	private readonly treeData: any = {};
+	private readonly descData: any = {};
 	private filteredTreeData: any = undefined;
 	public collapsibleState = vscode.TreeItemCollapsibleState.Collapsed;
 	private _onDidChangeTreeData: vscode.EventEmitter<NodeItem | undefined> = new vscode.EventEmitter<NodeItem | undefined>();
 	readonly onDidChangeTreeData: vscode.Event<NodeItem | undefined> = this._onDidChangeTreeData.event;
 
-	constructor(private context: vscode.ExtensionContext, dir: string) {
-		// 遍历abilitiesDir下面所有的文件
-		const files = fs.readdirSync(dir);
-		files.forEach(file => {
-			this.treeData[file] = {};
-		});
-		console.log(this.treeData);
-
+	constructor(private context: vscode.ExtensionContext) {
+		const gameDir = getGameDir();
+		this.treeData = this.readDirectory(path.join(gameDir, "scripts/npc"));
+		this.descData = readKeyValue2(fs.readFileSync(path.join(gameDir, "resource/addon_schinese.txt"), "utf-8")).lang.Tokens;
 	}
+
+	private readDirectory(dir: string): any {
+		const result: any = {};
+		const items = fs.readdirSync(dir, { withFileTypes: true });
+		items.forEach(item => {
+			if (item.isDirectory()) {
+				result[item.name] = this.readDirectory(path.join(dir, item.name));
+			} else {
+				result[item.name] = path.join(dir, item.name);
+			}
+		});
+		return result;
+	}
+
 	refresh(): void {
 		this._onDidChangeTreeData.fire(undefined);
 	}
+
 	rebuild(): void {
 		let temp = this.filteredTreeData ? this.filteredTreeData : this.treeData;
 		this.filteredTreeData = {};
@@ -38,50 +50,118 @@ export class KvEditorTreeProvider implements vscode.TreeDataProvider<NodeItem> {
 	}
 
 	getChildren(element?: NodeItem): Thenable<NodeItem[]> {
-		let apiTreeData = this.filteredTreeData ? this.filteredTreeData : this.treeData;
 		if (element) {
-			return Promise.resolve([]);
-		} else {
-			let classItems = [];
-			for (const className in apiTreeData) {
-				classItems.push(new NodeItem(className, vscode.TreeItemCollapsibleState.None, ItemType.class));
+			const children = element.filePath as Record<string, any>;
+			if (typeof children == "object") {
+				const childItems = [];
+				for (const childName in children) {
+					const isDirectory = typeof children[childName] === 'object';
+					if (isDirectory) {
+						childItems.push(new NodeItem(childName, vscode.TreeItemCollapsibleState.Collapsed, ItemType.folder, children[childName]));
+					} else {
+						childItems.push(new NodeItem(childName, vscode.TreeItemCollapsibleState.Collapsed, ItemType.file, children[childName]));
+					}
+				}
+				// 对 childItems 进行排序，文件夹在上面，文件在下面
+				childItems.sort((a, b) => {
+					if (a.itemType === ItemType.folder && b.itemType === ItemType.file) {
+						return -1;
+					} else if (a.itemType === ItemType.file && b.itemType === ItemType.folder) {
+						return 1;
+					} else {
+						return a.label.localeCompare(b.label);
+					}
+				});
+				return Promise.resolve(childItems);
+			} else if (element.itemType === ItemType.file) {
+				const childItems = [];
+				// 创建kv的子节点导航
+				const kv = readKeyValue2(fs.readFileSync(children, 'utf-8'));
+				const keys = kv[Object.keys(kv)[0]];
+				for (const key in keys) {
+					const descKey = Object.keys(this.descData).find(k => k.toLowerCase() === ("dota_tooltip_ability_" + key).toLowerCase())
+						?? Object.keys(this.descData).find(k => k.toLowerCase() === key.toLowerCase());
+					const desc = descKey ? this.descData[descKey] : undefined;
+					const tooltip = descKey ? this.descData[descKey + "_description"] : undefined;
+					childItems.push(new NodeItem(key, vscode.TreeItemCollapsibleState.None, ItemType.key, element.filePath, desc, tooltip));
+				}
+				return Promise.resolve(childItems);
+			} else {
+				return Promise.resolve([]);
 			}
-			return Promise.resolve(classItems);
+		} else {
+			let apiTreeData = this.filteredTreeData ? this.filteredTreeData : this.treeData;
+			// 根目录
+			const rootItems = [];
+			for (const rootName in apiTreeData) {
+				const isDirectory = typeof apiTreeData[rootName] === 'object';
+				if (isDirectory) {
+					rootItems.push(new NodeItem(rootName, vscode.TreeItemCollapsibleState.Expanded, ItemType.folder, apiTreeData[rootName]));
+				} else {
+					rootItems.push(new NodeItem(rootName, vscode.TreeItemCollapsibleState.Collapsed, ItemType.file, apiTreeData[rootName]));
+				}
+			}
+			// 对 rootItems 进行排序，文件夹在上面，文件在下面
+			rootItems.sort((a, b) => {
+				if (a.itemType === ItemType.folder && b.itemType === ItemType.file) {
+					return -1;
+				} else if (a.itemType === ItemType.file && b.itemType === ItemType.folder) {
+					return 1;
+				} else {
+					return a.label.localeCompare(b.label);
+				}
+			});
+			return Promise.resolve(rootItems);
 		}
 	}
 }
-// 类型
+
 enum ItemType {
-	class = 0,
-	function = 1,
-	constants = 2,
-	enumType = 3,
-	enum = 4,
+	folder = 0,
+	file = 1,
+	key = 2,
 }
+
 export class NodeItem extends vscode.TreeItem {
 	constructor(
 		public readonly label: string,	// 用来保存名字
 		public collapsibleState: vscode.TreeItemCollapsibleState,	// 折叠状态
 		public readonly itemType: ItemType,	// 类型
+		public readonly filePath: string | object,	// 文件路径
 		public readonly description?: string,	// 描述
 		public readonly tooltip?: string,	// tooltip
 		public readonly command?: vscode.Command	// 注册指令
 	) {
 		super(label, collapsibleState);
-		if (this.itemType === ItemType.function) {
-			this.iconPath = path.join(__filename, '..', '..', 'images', 'function.svg');
-			this.contextValue = 'NodeItem';
-		} else if (this.itemType === ItemType.class) {
+		if (this.itemType === ItemType.folder) {
+			this.iconPath = vscode.ThemeIcon.Folder;
+		} else if (this.itemType === ItemType.key) {
+			this.iconPath = new vscode.ThemeIcon("symbol-method");
+			this.command = {
+				command: 'extension.openFileAtPosition',
+				title: 'Open File at Position',
+				arguments: [this.filePath, this.label]
+			};
+		} else if (this.itemType === ItemType.file) {
 			this.iconPath = path.join(__filename, '..', '..', 'images', 'kv.svg');
-			this.contextValue = 'Class';
-		} else if (this.itemType === ItemType.enumType) {
-			this.iconPath = path.join(__filename, '..', '..', 'images', 'enum_type.svg');
-			this.contextValue = 'Class';
-		} else if (this.itemType === ItemType.constants) {
-			this.iconPath = path.join(__filename, '..', '..', 'images', 'enum_type.svg');
-		} else if (this.itemType === ItemType.enum) {
-			this.iconPath = path.join(__filename, '..', '..', 'images', 'enum.svg');
-			this.contextValue = 'NodeItem';
+			this.command = {
+				command: 'vscode.open',
+				title: 'Open File',
+				arguments: [vscode.Uri.file(this.filePath as string)]
+			};
 		}
 	}
 }
+
+// 注册命令
+vscode.commands.registerCommand('extension.openFileAtPosition', async (filePath: string, key: string) => {
+	const document = await vscode.workspace.openTextDocument(filePath);
+	const editor = await vscode.window.showTextDocument(document);
+	const text = document.getText();
+	const position = text.indexOf(key);
+	if (position !== -1) {
+		const startPosition = document.positionAt(position);
+		editor.selection = new vscode.Selection(startPosition, startPosition);
+		editor.revealRange(new vscode.Range(startPosition, startPosition));
+	}
+});
--- src/module/kvEditor.ts
+++ src/module/kvEditor.ts
@@ -1,31 +1,12 @@
 import * as vscode from 'vscode';
-import * as path from 'path';
 import { KvEditorTreeProvider } from '../TreeDataProvider/kvTree';
-import { getGameDir } from './addonInfo';
-import { window } from 'vscode';
 
-let abilitiesKvEditorTree: KvEditorTreeProvider;
-let unitsKvEditorTree: KvEditorTreeProvider;
-let itemsKvEditorTree: KvEditorTreeProvider;
-let configsKvEditorTree: KvEditorTreeProvider;
+let kvEditorTree: KvEditorTreeProvider;
 
 /** 树状图模块 */
 export async function kvEditorInit(context: vscode.ExtensionContext) {
-	const gameDir = getGameDir();
-	if (abilitiesKvEditorTree === undefined) {
-		abilitiesKvEditorTree = new KvEditorTreeProvider(context, path.join(gameDir, "scripts/npc/kv/abilities"));
-		vscode.window.registerTreeDataProvider('ability_kv', abilitiesKvEditorTree);
-	}
-	if (unitsKvEditorTree === undefined) {
-		unitsKvEditorTree = new KvEditorTreeProvider(context, path.join(gameDir, "scripts/npc/kv/units"));
-		vscode.window.registerTreeDataProvider('unit_kv', unitsKvEditorTree);
-	}
-	if (itemsKvEditorTree === undefined) {
-		itemsKvEditorTree = new KvEditorTreeProvider(context, path.join(gameDir, "scripts/npc/kv/items"));
-		vscode.window.registerTreeDataProvider('item_kv', itemsKvEditorTree);
-	}
-	if (configsKvEditorTree === undefined) {
-		configsKvEditorTree = new KvEditorTreeProvider(context, path.join(gameDir, "scripts/npc/kv/gameplay"));
-		vscode.window.registerTreeDataProvider('kv_config', configsKvEditorTree);
+	if (kvEditorTree === undefined) {
+		kvEditorTree = new KvEditorTreeProvider(context);
+		vscode.window.registerTreeDataProvider('kv_explorer', kvEditorTree);
 	}
 }
